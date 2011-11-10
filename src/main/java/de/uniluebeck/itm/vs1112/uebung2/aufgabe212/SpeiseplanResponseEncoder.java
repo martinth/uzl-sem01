dec968b4fcf8c9bfc7fb5921a047ed976c4b1871
Martin Thurau <martin.thurau@gmail.com>
2011-11-10 15:41:39 +0100

    Encoder für Request und Respsone - erste, ungetestete Version

--- src/main/java/de/uniluebeck/itm/vs1112/uebung2/aufgabe212/SpeiseplanResponseEncoder.java
+++ src/main/java/de/uniluebeck/itm/vs1112/uebung2/aufgabe212/SpeiseplanResponseEncoder.java
@@ -1,5 +1,7 @@
 package de.uniluebeck.itm.vs1112.uebung2.aufgabe212;
 
+import java.nio.ByteBuffer;
+
 import de.uniluebeck.itm.vs1112.uebung2.Encoder;
 import de.uniluebeck.itm.vs1112.uebung2.EncodingException;
 
@@ -7,6 +9,42 @@ public class SpeiseplanResponseEncoder implements Encoder<SpeiseplanResponse> {
 
 	@Override
 	public byte[] encode(final SpeiseplanResponse object) throws EncodingException {
-		return new byte[0];  // TODO implement
+		
+		ByteBuffer buf = ByteBuffer.allocate(1000); //FIXME
+		buf.put((byte) 1);
+		
+		if (object.getDayOfYear() < 1 || object.getDayOfYear() > 366) {
+			throw new EncodingException("DayOfYear is out of range");
+		}
+		buf.putShort((short) object.getDayOfYear());
+		
+		int dishCount = object.getSpeiseplan().getSpeisen().size();
+		if (dishCount < 0 || dishCount > 3) {
+			throw new EncodingException("DishCount is out of range");
+		}
+		buf.put((byte) dishCount);
+		
+		int i = 0;
+		for (Speise speise : object.getSpeiseplan().getSpeisen()) {
+			if (speise.getPriceInCent() < 0) {
+				throw new EncodingException(String.format(
+					"Price in meal %d is out of range", i)
+				);
+			}
+			buf.putShort((short) speise.getPriceInCent());
+			
+			if (speise.getDescription().length() > 255) {
+				throw new EncodingException(String.format(
+					"Description in meal %d is to long", i)
+				);
+			}
+			buf.put((byte) speise.getDescription().length());
+			buf.put(speise.getDescription().getBytes());
+		}
+		
+		byte[] retBuf = new byte[buf.position()];
+		System.arraycopy(buf.array(), 0, retBuf, 0, buf.position());
+		
+		return retBuf;
 	}
 }
