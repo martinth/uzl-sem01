305f5cfa75a2981aed07cdd7ab11c664e88c9b80
Martin Thurau <martin.thurau@gmail.com>
2011-11-10 16:23:25 +0100

    Änderungen wieder eingefügt

--- vs_uebung02/src/main/java/de/uniluebeck/itm/vs1112/uebung2/aufgabe212/SpeiseplanRequestEncoder.java
+++ vs_uebung02/src/main/java/de/uniluebeck/itm/vs1112/uebung2/aufgabe212/SpeiseplanRequestEncoder.java
@@ -1,5 +1,7 @@
 package de.uniluebeck.itm.vs1112.uebung2.aufgabe212;
 
+import java.nio.ByteBuffer;
+
 import de.uniluebeck.itm.vs1112.uebung2.Encoder;
 import de.uniluebeck.itm.vs1112.uebung2.EncodingException;
 
@@ -7,6 +9,16 @@ public class SpeiseplanRequestEncoder implements Encoder<SpeiseplanRequest> {
 
 	@Override
 	public byte[] encode(final SpeiseplanRequest object) throws EncodingException {
-		return new byte[0];  // TODO implement
+		
+		ByteBuffer buf = ByteBuffer.allocate(3);
+		buf.put((byte) 0);
+		
+		if (object.getDayOfYear() < 1 || object.getDayOfYear() > 366) {
+			throw new EncodingException("DayOfYear is out of range");
+		}
+		buf.putShort((short) object.getDayOfYear());
+		
+		
+		return buf.array();
 	}
 }
