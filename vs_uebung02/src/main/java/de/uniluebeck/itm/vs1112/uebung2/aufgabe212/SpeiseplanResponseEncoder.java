00b4abb170a5aa17c7e5dd7096c0fb2fa4ef0331
Martin Thurau <martin.thurau@gmail.com>
2011-11-10 16:14:50 +0100

    Richtiges Basisprojekt importiert

---
+++ vs_uebung02/src/main/java/de/uniluebeck/itm/vs1112/uebung2/aufgabe212/SpeiseplanResponseEncoder.java
@@ -0,0 +1,12 @@
+package de.uniluebeck.itm.vs1112.uebung2.aufgabe212;
+
+import de.uniluebeck.itm.vs1112.uebung2.Encoder;
+import de.uniluebeck.itm.vs1112.uebung2.EncodingException;
+
+public class SpeiseplanResponseEncoder implements Encoder<SpeiseplanResponse> {
+
+	@Override
+	public byte[] encode(final SpeiseplanResponse object) throws EncodingException {
+		return new byte[0];  // TODO implement
+	}
+}
