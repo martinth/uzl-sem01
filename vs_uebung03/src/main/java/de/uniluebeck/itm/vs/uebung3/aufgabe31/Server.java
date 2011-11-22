6cbc64f196902b8a0249c98622f0bbfc0d8decce
Martin Thurau <martin.thurau@gmail.com>
2011-11-23 00:04:27 +0100

    Verteile Systeme 3.1: Läuft

--- vs_uebung03/src/main/java/de/uniluebeck/itm/vs/uebung3/aufgabe31/Server.java
+++ vs_uebung03/src/main/java/de/uniluebeck/itm/vs/uebung3/aufgabe31/Server.java
@@ -1,12 +1,34 @@
 package de.uniluebeck.itm.vs.uebung3.aufgabe31;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+import de.uniluebeck.itm.vs.uebung3.apps.SpeiseplanDatabase;
+import de.uniluebeck.itm.vs.uebung3.serialization.DecodingException;
+import de.uniluebeck.itm.vs.uebung3.serialization.EncodingException;
+import de.uniluebeck.itm.vs.uebung3.serialization.elementwise.SpeiseplanRequestDecoder;
+import de.uniluebeck.itm.vs.uebung3.serialization.elementwise.SpeiseplanResponseEncoder;
+import de.uniluebeck.itm.vs.uebung3.types.Speiseplan;
+import de.uniluebeck.itm.vs.uebung3.types.SpeiseplanRequest;
+import de.uniluebeck.itm.vs.uebung3.types.SpeiseplanResponse;
 
 public class Server implements Runnable{
 
+    private InputStream in;
+    private OutputStream out;
+    private SpeiseplanRequestDecoder decoder;
+    private SpeiseplanResponseEncoder encoder;
+
     public Server(InputStream in, OutputStream out){
-        //TODO: Implement constructor, define and initialize member variables
+        this.in = in;
+        this.out = out;
+        
+        decoder = new SpeiseplanRequestDecoder();
+        encoder = new SpeiseplanResponseEncoder();
     }
 
     /**
@@ -14,9 +36,45 @@ public class Server implements Runnable{
      * Use SpeiseplanDatabase.getSpeiseplan(int dayOfYear) to determine the actual dishes.
      */
     public void run() {
-        /*
-        TODO: Implement server-application to read one requests from the InputStream and write the response
-        to the OutputStream given as constructor parameters
-        */
+        try {
+            // read header and determine length
+            ByteBuffer buf = ByteBuffer.allocate(4);
+            for(int i= 0; i <4; i++) {
+                buf.put((byte) in.read());
+            }
+            int length = buf.getInt(0);
+            
+            // read data
+            ByteBuffer inputBuffer = ByteBuffer.allocate(length);
+            for(int i= 0; i<length; i++) {
+                inputBuffer.put((byte) in.read());
+            }
+
+            // get and encode response
+            SpeiseplanRequest request = decoder.decode(inputBuffer.array());
+            Speiseplan plan = SpeiseplanDatabase.getSpeiseplan(request.getDayOfYear());
+            byte[] response = encoder.encode(new SpeiseplanResponse(request.getDayOfYear(), plan));
+            
+            // prepare length header
+            ByteBuffer outputBuffer = ByteBuffer.allocate(4 + response.length);
+            outputBuffer.putInt(response.length);
+            outputBuffer.put(response);
+            
+            // send data
+            out.write(outputBuffer.array());
+            
+        } catch (IOException e) {
+            // can't do anything useful, let finally clean up
+        } catch (DecodingException e) {
+            // can't do anything useful, let finally clean up
+        } catch (EncodingException e) {
+            // can't do anything useful, let finally clean up
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                // what the hell, if close() fails there are probably bigger problems
+            }
+        }
     }
 }
