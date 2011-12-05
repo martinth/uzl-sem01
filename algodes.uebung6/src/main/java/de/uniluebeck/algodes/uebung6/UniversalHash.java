46f71157d05c216b04060c9bbdd4aa554fe14bad
Martin Thurau <martin.thurau@gmail.com>
2011-12-05 12:32:58 +0100

    AlgoDes 6: UniversalHash gefixt + logging

--- algodes.uebung6/src/main/java/de/uniluebeck/algodes/uebung6/UniversalHash.java
+++ algodes.uebung6/src/main/java/de/uniluebeck/algodes/uebung6/UniversalHash.java
@@ -3,12 +3,18 @@ package de.uniluebeck.algodes.uebung6;
 import java.math.BigInteger;
 import java.util.Random;
 
+import org.apache.log4j.Logger;
+
+import com.google.common.base.Objects;
+
 /**
  * Universal hash function. Implements H_p
  * @author martin
  *
  */
 public class UniversalHash implements Hashfunction {
+	
+	Logger log = Logger.getLogger(this.getClass().getCanonicalName());
 	
 	/**
 	 * a large prime number used in the current family
@@ -24,31 +30,33 @@ public class UniversalHash implements Hashfunction {
 	private UniversalHash(long familyBase) {
 		this.p = familyBase;
 		calcFamilyBases();
+		log.info("Created new "+ this +" based on given base "+ familyBase);
 	}
 
 	public UniversalHash() {
 		/* create a prime number with Long.SIZE bitlength for use
 		 * as p for the current family. We use a long, since p must be larger
 		 * than the maximum key */
-		p = BigInteger.probablePrime(Long.SIZE, random).longValue();
-		
+		p = BigInteger.probablePrime(Long.SIZE-1, random).longValue() % Long.MAX_VALUE;
 		/* calc a and b based on p */
 		calcFamilyBases();
+		
+		log.info("Created new "+ this);
 	}
 
 	/**
 	 * Chooses a random a and b for this instance with 0 < a,b < p-1.
 	 */
 	private void calcFamilyBases() {
-		a = (1 + random.nextLong()) % p-1;
-		assert(a != 0);
+		a = Math.abs((1 + random.nextLong()) % p-1);
+		assert(a > 0);
 		assert(a < p-1);
 		
-		b = (1 + random.nextLong()) % p-1;
-		assert(b != 0);
+		b = Math.abs((1 + random.nextLong()) % p-1);
+		assert(b > 0);
 		assert(b < p-1);
 	}
-
+	
 	public int hash(int key, int size) {
 		/* calculate the hash bases on the values of a,b and p:
 		 * 		h_a,b(x) = ((ax+b) mod p) mod r
@@ -60,5 +68,14 @@ public class UniversalHash implements Hashfunction {
 	public Hashfunction createNext() {
 		return new UniversalHash(p);
 	}
+	
+	public String toString() {
+		return Objects.toStringHelper(this)
+				.add("p", p)
+				.add("a", a)
+				.add("b", b)
+				.toString();
+	}
+
 
 }
---
+++ algodes.uebung6/src/test/java/de/uniluebeck/algodes/uebung6/UniversalHashTest.java
@@ -0,0 +1,25 @@
+package de.uniluebeck.algodes.uebung6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UniversalHashTest {
+
+	@Test
+	public void testUniversalHash() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHash() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNext() {
+		Hashfunction uH = new UniversalHash();
+		uH.createNext();
+	}
+
+}
