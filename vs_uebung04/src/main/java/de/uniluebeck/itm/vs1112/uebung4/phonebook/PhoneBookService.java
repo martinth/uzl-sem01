99b35cc0b152084da8786a73967221d55b318ba9
Martin Thurau <martin.thurau@gmail.com>
2011-12-12 21:29:52 +0100

    VS 4: Soweit fertig sind \"GET /phonebook\", \"GET /phonebook/id\" und \"PUT /phonebook/id\"

--- vs_uebung04/src/main/java/de/uniluebeck/itm/vs1112/uebung4/phonebook/PhoneBookService.java
+++ vs_uebung04/src/main/java/de/uniluebeck/itm/vs1112/uebung4/phonebook/PhoneBookService.java
@@ -1,17 +1,34 @@
 package de.uniluebeck.itm.vs1112.uebung4.phonebook;
 
+import java.net.URISyntaxException;
+import java.util.Set;
+
 import de.uniluebeck.itm.vs1112.uebung4.employeedb.EmployeeDB;
+import de.uniluebeck.itm.vs1112.uebung4.employeedb.EmployeeDBEntry;
+import de.uniluebeck.itm.vs1112.uebung4.employeedb.EmployeeDBIdAlreadyExistsException;
+import de.uniluebeck.itm.vs1112.uebung4.employeedb.EmployeeDBUnknownIdException;
+import de.uniluebeck.itm.vs1112.uebung4.xml.PhoneBookEntry;
+import de.uniluebeck.itm.vs1112.uebung4.xml.PhoneBookEntryList;
+import de.uniluebeck.itm.vs1112.uebung4.xml.PhoneBookEntryWithUri;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.apache.log4j.Logger;
 
 /**
  * Skeleton class to implement the phone book REST service specification.
  */
 @Path("/phonebook")
 public class PhoneBookService {
+    
+    private final Logger log = Logger.getLogger(this.getClass().getCanonicalName());
 
 	/**
 	 * Reference to the legacy employee DB. Will be set by the framework upon program (or unit-test) startup.
@@ -20,9 +37,83 @@ public class PhoneBookService {
 	private EmployeeDB employeeDB;
 
 	@GET
-	public Response get() {
-		// TODO implement
-		return null;
+	@Produces("text/xml")
+	
+	public Response getPhonebook() throws EmployeeDBIdAlreadyExistsException, EmployeeDBUnknownIdException {
+	    Set<EmployeeDBEntry> entries = employeeDB.getEntries();
+	    if(entries.size() == 0) {
+	        return Response.noContent().build();
+	    }
+	    
+	    PhoneBookEntryList respData = new PhoneBookEntryList();
+	    
+	    for (EmployeeDBEntry dbEntry : entries) {
+	        PhoneBookEntry pbe = new PhoneBookEntry();
+	        pbe.setName(dbEntry.getName());
+	        pbe.setNumber(dbEntry.getPhoneNumber());
+	        
+	        PhoneBookEntryWithUri pbu = new PhoneBookEntryWithUri();
+	        pbu.setPhoneBookEntry(pbe);
+	        pbu.setUri("/phonebook/"+dbEntry.getEmployeeId());
+	        respData.getPhoneBookEntryWithUri().add(pbu);
+        }
+	    
+	    return Response.ok(respData).build();
 	}
+	
+	@GET
+	@Path("/{id: [0-9]+}")
+	@Produces("application/xml")
+	public Response getEntry(@PathParam("id") int id) {
+	    log.debug("GET phonebook entry with id "+id);
+	    EmployeeDBEntry employee = employeeDB.findById(id);
+	    if(employee == null || employee.getPhoneNumber() == null || employee.getPhoneNumber().isEmpty()) {
+	        log.debug("phonebook entry with id "+id+" wasn't found:"+ employee);
+	        return Response.noContent().build();
+	    } else {
+	        PhoneBookEntry pbe = new PhoneBookEntry();
+	        pbe.setName(employee.getName());
+	        pbe.setNumber(employee.getPhoneNumber());
+	        log.debug("phonebook entry with id "+id+" found:\n"+pbe);
+	        return Response.ok(pbe).build();
+	    }
+	}
+	
+	@PUT
+    @Path("/{id: [0-9]+}")
+	@Consumes("application/xml")
+    @Produces("application/xml")
+    public Response putEntry(
+            @PathParam("id") int id,
+            PhoneBookEntry newEntry
+    ) throws EmployeeDBUnknownIdException, // can only happen if employeeDB is not thread save
+    EmployeeDBIdAlreadyExistsException, URISyntaxException {   // should not happen in this universe 
+        log.debug("PUT phonebook entry with id "+id+" data:\n"+newEntry);
+	    
+        EmployeeDBEntry employee = employeeDB.findById(id);
+        Status status = null;
+        
+        // employee doesn't exist -> create it
+        if(employee == null) {
+            employee = employeeDB.create(id, newEntry.getName());
+            status = Status.CREATED;
+            log.debug("Employee not in DB. Was created");
+        }
+        // employee is already in db -> update
+        else {
+            employee.setName(newEntry.getName());
+            status = Status.OK;
+            log.debug("Employee in DB. Was updated");
+        }
+        employee.setPhoneNumber(newEntry.getNumber());
+        employeeDB.update(employee);
+        return Response.status(status).entity(newEntry).build();
+    }
+	
+	
+	
+
+	
+	
 
 }
